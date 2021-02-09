6317f06999bdc47c640d12c7f2a9851bbb8e6f40
Sergio Vaquero Garcia <svg26@msn.com>
2021-02-09 12:25:22 +0100

    VERSIÓN INICIAL

--- geometria/src/Circulo.java
+++ geometria/src/Circulo.java
@@ -1,7 +1,7 @@
 
 public class Circulo extends FiguraGeometrica {
 	private double radio;
-	//Constante pi
+	//Constante pi 
 	static final double PI = 3.1416;
 	
 	public Circulo(double r, String tipoFigura) {
