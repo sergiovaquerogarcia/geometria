a7f9d1c3647aa088d4141c060275dd1b257c3412
Sergio Vaquero Garcia <svg26@msn.com>
2021-02-09 12:49:11 +0100

    VERSIÓN REFACTORIZADA

--- geometria/src/Rectangulo_SVG.java
+++ geometria/src/Rectangulo_SVG.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_SVG extends FiguraGeometrica_SVG {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_SVG(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
