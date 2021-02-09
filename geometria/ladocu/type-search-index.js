8716e2a26a1fa9bc63aa085ee3a22071111a030b
Sergio Vaquero Garcia <svg26@msn.com>
2021-02-09 13:25:51 +0100

    Docuentar es de sabios, aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_SVG"},{"p":"<Unnamed>","l":"FiguraGeometrica_SVG"},{"p":"<Unnamed>","l":"Rectangulo_SVG"},{"p":"<Unnamed>","l":"Triangulo_SVG"}]
